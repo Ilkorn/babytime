89c39027706b1109aece4cac9dea5475f0803a00
Ilkorn <korneev.box@gmail.com>
2014-04-12 13:43:08 +0300

    init rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babytime::Application.config.secret_token = '70304ee653e7c0f9d23b4aadeab5ba894ee48ebfb1e08ad4ed90b65fb58bb7cc84bef9b25c2021cab548166318c340e021f9aafbc88ea8e190f318b01556a03d'
